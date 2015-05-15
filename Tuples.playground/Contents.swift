6ffb7987a5cb91f5134b58b5847133558c98a5f0
harolei <harolei@gmail.com>
2015-05-15 10:42:27 +0800

    modified the calcTipWithTipPct method to return tuple

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -1,22 +1,37 @@
-let tipAndTotal:(Double, Double) = (4.00, 25.19)
+import Foundation
 
-tipAndTotal.0
-tipAndTotal.1
-
-let (theTipAmt, theTotal) = tipAndTotal
-theTipAmt
-theTotal
-
-let tipAndTotalNamed = (tipAmt:4.00, total:25.19)
-tipAndTotalNamed.tipAmt
-tipAndTotalNamed.total
-
-let total = 21.19
-let taxPct = 0.06
-let subtotal = total / (taxPct + 1)
-func calcTipWithTipPct(tipPct:Double) -> (tipAmt:Double, total:Double) {
-    let tipAmt = subtotal * tipPct
-    let finalTotal = total + tipAmt
-    return (tipAmt, finalTotal)
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> (tipAmt:Double, total:Double) {
+        let tipAmt = subtotal * tipPct
+        let finalTotal = total + tipAmt
+        return (tipAmt, finalTotal)
+    }
+    
+    func returnPossibleTips() -> [Int: (tipAmt:Double, total:Double)] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        
+        var retval = Dictionary<Int, (tipAmt:Double, total:Double)>()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        
+        return retval
+    }
 }
-calcTipWithTipPct(0.20)
+
